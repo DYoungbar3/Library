727cb0b9621eaf581f7c6e6b9522c2b580667649
Daniel Youngbar <d.youngbar928@gmail.com>
2022-12-26 13:55:17 -0800

    Get started

---
+++ index.js
@@ -0,0 +1,34 @@
+let myLibrary = ["The Hobbit", "Uncrowned", "Awesome Sauce"];
+const mainContainer = document.querySelector(".container");
+const addBookBtn = document.querySelector(".newBook");
+const form = document.querySelector('.form');
+const submitBtn = document.querySelector('.submit');
+
+form.style.display = 'none';
+
+addBookBtn.addEventListener('click', () => {
+    form.style.display = 'grid';
+})
+
+submitBtn.addEventListener('click', () => {
+    const title = document.querySelector('#title');
+    myLibrary.push(title)
+    addBookToLibrary(myLibrary)
+    
+})
+
+function Book (title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary (library) {
+    for (i in library) {
+        const tag = document.createElement('section')
+        const info = document.createTextNode(library[i])
+        tag.appendChild(info)
+        mainContainer.appendChild(tag)
+    }
+}
